7d39796a3a6d3d047ce11331e0fdc916cb1b5537
Tracy Parsons <ventolinman@gmail.com>
2014-04-23 17:47:59 -0400

    Update wrapper.pl

--- CAPSTONE_PROJECT/wrapper.pl
+++ CAPSTONE_PROJECT/wrapper.pl
@@ -1,7 +1,17 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
-#use Data::Dumper;
+
+require "FB_Link_Generator.pl";
+require "FB_Link_Scraper.pl";
+require "FB_Scraper.pl";
+require "GP_Link_Generator.pl";
+require "GP_Link_Scraper.pl";
+#require "GP_Scraper.pl";
+require "LI_Link_Generator.pl";
+require "LI_Scraper.pl";
+require "ZI_Link_Generator.pl";
+
 #Employment data project
 
 ###Data Munging###
@@ -13,7 +23,7 @@ while (my $cmdarg = shift) {
 	#print "Opening file $cmdarg;\n";
 	open FILE, "<$cmdarg" or die $!;
 	while (<FILE>) { push @lines, $_; }
-	close(FILE);
+	close FILE;
 }
 
 my @words;
@@ -35,36 +45,39 @@ foreach my $line (@lines){
 	push(@words,\%data);
 }
 
-system ("rm OUTPUT.dat") if -e "OUTPUT.dat";
-open FILE, ">OUTPUT.dat";
-print FILE "<?xml version=\"1.0\" encoding = \"UTF-8\"?>\n<data>\n";
 foreach my $person (@words){
 	my $fname = $person->{firstname};
 	my $mname = $person->{middlename};
 	my $lname = $person->{lastname};
 	# NOTE: This stuff will have to change if we transition to a windows environment
-	#system ("rm ./profiles/$fname-$lname.dat") if -e "./profiles/$fname-$lname.dat";
-	print FILE "<person name = \"". $fname ." ". $mname ." ". $lname ."\">\n";  	
-	print FILE "	<profile site=\"FaceBook\">\n";
-	system ("echo `perl FB_Link_Generator.pl $fname $lname` >> OUTPUT.dat");
-	wait;	
-	print FILE "	</profile>\n";
-	#system ("echo -n `perl FB_Scraper.pl $fname $lname` >> OUTPUT.dat");
-	print FILE "	<profile site=\"LinkedIn\">\n";
-	#`echo -n " " >> ./profiles/$fname-$lname.dat`;
-	system ("echo `perl LI_Link_Generator.pl $fname $lname` >> OUTPUT.dat");
-	wait;	
-	#system ("echo -n `perl LI_Scraper.pl $fname $lname` >> OUTPUT.dat");
-	print FILE "	</profile>\n";
+	system ("rm ./profiles/$fname-$lname.dat") if -e "./profiles/$fname-$lname.dat";
+	print "$fname,$mname,$lname";
+	my $FBLinks = 0; $FBLinks = FB_Link_Generator ($fname, $lname);
+	if ($FBLinks) {
+		print $FBLinks;
+		my $FBProfiles = 0; $FBProfiles = FB_Link_Scraper ($fname, $lname, $FBLinks);
+		if ($FBProfiles) {
+			print $FBProfiles;
+			my $FBResults = 0; $FBResults = FB_Scraper ($fname, $lname, $FBProfiles);
+			if ($FBResults) { print $FBResults; }
+		}
+	}
+	my $GPLinks = GP_Link_Generator ($fname, $lname);
+	#my $GPLinks = ", https://www.google.com/#q=$fname+$lname+ohio+state+university+google+plus";
+	if ($GPLinks) {
+		print $GPLinks;
+		my $GPProfiles = 0;
+		$GPProfiles = GP_Link_Scraper ($fname, $lname, $GPLinks);
+		if ($GPProfiles) { print $GPProfiles; }
+	}
 	
-	#`echo -n " " >> ./profiles/$fname-$lname.dat`;
-	print FILE "	<profile site=\"GooglePlus\">\n";
-	system ("echo `perl GP_Link_Generator.pl $fname $lname` >> OUTPUT.dat");
-	wait;	
-	print FILE "	</profile>\n";
-	#system ("echo -n `perl GP_Scraper.pl $fname $lname` >> OUTPUT.dat");
-	print FILE "</person>\n";
+	my $LILinks = LI_Link_Generator ($fname, $lname);
+	if ($LILinks) { print $LILinks; }
+	
+	my $ZILinks = ZI_Link_Generator ($fname, $lname, "dummycode");	# for this to work, "dummycode" will need to be replaced with partner code from ZoomInfo 
+	if ($ZILinks) { print $ZILinks; }
+
+	print "\n";
 }
-print FILE "</data>\n";
-close FILE;
-system ("chmod 755 OUTPUT.dat");
+
+
